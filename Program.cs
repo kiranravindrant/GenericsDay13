ed0253ab0d146f13d2cc3e875332f43664810e54
kiranravindrant <kiranravindrant@gmail.com>
2021-07-23 22:44:00 +0530

    [kiran]Added:MainMethod of program to find max of 3 integers

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace FindMax
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Find Maximum Number");
+            int test1=MaxNumber.ReturnMaxInteger(9, 7, 5);
+            int test2= MaxNumber.ReturnMaxInteger(7, 9, 5);
+            int test3= MaxNumber.ReturnMaxInteger(7, 5, 9);
+            Console.WriteLine($"Max value is {test1}");
+            Console.WriteLine($"Max value is {test2}");
+            Console.WriteLine($"Max value is {test3}");
+
+
+
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
