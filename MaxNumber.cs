d2eed2c16e80ae12e7025ce11549b1d42f24cffc
kiranravindrant <kiranravindrant@gmail.com>
2021-07-24 00:01:31 +0530

    [kiran]Added:class with method to return max of float value

--- MaxNumber.cs
+++ MaxNumber.cs
@@ -7,44 +7,61 @@ namespace FindMax
     public class MaxNumber
     {
 
-      
+
         public static int ReturnMaxInteger(int firstVal, int secondVal, int thirdVal)
         {
-            
-                if (firstVal.CompareTo(secondVal) > 0 && firstVal.CompareTo(thirdVal) > 0)
-                {
-                    return firstVal;
+            #region
+            if (firstVal.CompareTo(secondVal) > 0 && firstVal.CompareTo(thirdVal) > 0)
+            {
+                return firstVal;
 
-                }
-                else if (secondVal.CompareTo(firstVal) > 0 && secondVal.CompareTo(thirdVal) > 0)
-                {
-                    return secondVal;
+            }
+            else if (secondVal.CompareTo(firstVal) > 0 && secondVal.CompareTo(thirdVal) > 0)
+            {
+                return secondVal;
 
-                }
-                else if (thirdVal.CompareTo(firstVal) > 0 && thirdVal.CompareTo(secondVal) > 0)
-                {
-                    return thirdVal;
+            }
+            else if (thirdVal.CompareTo(firstVal) > 0 && thirdVal.CompareTo(secondVal) > 0)
+            {
+                return thirdVal;
 
-                }
-              throw new Exception("Equal Numbers present");   // so as to provide an exception to
-                                                                //return type of the method,as its not
-                                                                //returning an integer value.
-            
-           
-
-
-
-
-
-          
-
-                
-           
-              
-            
-
+            }
+            throw new Exception("Equal Numbers present");   // so as to provide an exception to
+                                                            //return type of the method,as its not
+                                                            //returning an integer value.
 
         }
+        #endregion
+
+        public static double ReturnMaxFloat(double firstVal, double secondVal, double thirdVal)
+        {
+          #region
+            if (firstVal.CompareTo(secondVal) > 0 && firstVal.CompareTo(thirdVal) > 0)
+            {
+                return firstVal;
+            }
+            if (secondVal.CompareTo(firstVal) > 0 && secondVal.CompareTo(thirdVal) > 0)
+            {
+                return secondVal;
+            }
+            if (thirdVal.CompareTo(firstVal) > 0 && thirdVal.CompareTo(secondVal) > 0)
+            {
+                return thirdVal;
+            }
+            throw new Exception("Similar numbers present!!!!");
+        }
+        #endregion
+
+
+        
+
+
+
+
+
+
+
+
 
 
     }
