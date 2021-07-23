9cd462de187bea3dca981277d5d43cf13d0a7c73
kiranravindrant <kiranravindrant@gmail.com>
2021-07-23 22:45:11 +0530

    [kiran]Added:Class with method to find max of 3 integers and return it

---
+++ MaxNumber.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FindMax
+{
+    public class MaxNumber
+    {
+
+      
+        public static int ReturnMaxInteger(int firstVal, int secondVal, int thirdVal)
+        {
+            
+                if (firstVal.CompareTo(secondVal) > 0 && firstVal.CompareTo(thirdVal) > 0)
+                {
+                    return firstVal;
+
+                }
+                else if (secondVal.CompareTo(firstVal) > 0 && secondVal.CompareTo(thirdVal) > 0)
+                {
+                    return secondVal;
+
+                }
+                else if (thirdVal.CompareTo(firstVal) > 0 && thirdVal.CompareTo(secondVal) > 0)
+                {
+                    return thirdVal;
+
+                }
+              throw new Exception("Equal Numbers present");   // so as to provide an exception to
+                                                                //return type of the method,as its not
+                                                                //returning an integer value.
+            
+           
+
+
+
+
+
+          
+
+                
+           
+              
+            
+
+
+        }
+
+
+    }
+}
+
